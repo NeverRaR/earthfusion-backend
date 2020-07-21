eecd26afb99a211ae21aceb74e227802b4e75d30
Marshmallow Nemo <xiongnemo@126.com>
2020-07-21 22:16:05 +0800

    fix issue: "The code must not contain multiple blank lines in a row" in Startup.cs:39

--- Startup.cs
+++ Startup.cs
@@ -36,7 +36,6 @@ namespace earthfusion_backend
             });
         }
 
-
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
