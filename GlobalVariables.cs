e9d975a42c591151497374ee07e80cead3e0919f
Marshmallow Nemo <xiongnemo@126.com>
2020-08-12 14:52:26 +0800

    add comment: GlobalVariables.cs

--- GlobalVariables.cs
+++ GlobalVariables.cs
@@ -1,7 +1,9 @@
 namespace OracleTest
 {
+    // use 'static' keyword to create 'global' variables
     public static class Globals
     {
+        // constructor set to use predefined list to init.
         public static KnownTables knownTables = new KnownTables();
     }
 }
