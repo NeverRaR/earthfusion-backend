286c3cc835df2b2ff038c8ee4c557a6eb0687214
Marshmallow Nemo <xiongnemo@126.com>
2020-08-19 20:15:24 +0800

    feat: use ini file for config

--- GlobalVariables.cs
+++ GlobalVariables.cs
@@ -1,3 +1,8 @@
+using Utils;
+using Microsoft.Extensions.Configuration.Ini;
+using System.Collections.Generic;
+using System;
+
 namespace OracleTest
 {
     // use 'static' keyword to create 'global' variables
@@ -5,5 +10,13 @@ namespace OracleTest
     {
         // constructor set to use predefined list to init.
         public static KnownTables knownTables = new KnownTables();
+    }
+}
+
+namespace earthfusion_backend
+{
+    public static class Globals
+    {
+        public static IDictionary<String,String> config = new IniLoader("config.ini").data;
     }
 }
