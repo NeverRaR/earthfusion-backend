7ecf44639a67a3a747813abc8be1f6d5ec018952
Marshmallow Nemo <xiongnemo@126.com>
2020-08-19 20:20:30 +0800

    code format: "A C# document may only contain a single namespace." at GlobalVariables.cs:16, "A C# document may only contain a single class at the root level unless all of the classes are partial and are of the same type." at GlobalVariables.cs:18

--- GlobalVariables.cs
+++ GlobalVariables.cs
@@ -1,8 +1,3 @@
-using Utils;
-using Microsoft.Extensions.Configuration.Ini;
-using System.Collections.Generic;
-using System;
-
 namespace OracleTest
 {
     // use 'static' keyword to create 'global' variables
@@ -10,13 +5,5 @@ namespace OracleTest
     {
         // constructor set to use predefined list to init.
         public static KnownTables knownTables = new KnownTables();
-    }
-}
-
-namespace earthfusion_backend
-{
-    public static class Globals
-    {
-        public static IDictionary<String,String> config = new IniLoader("config.ini").data;
     }
 }
