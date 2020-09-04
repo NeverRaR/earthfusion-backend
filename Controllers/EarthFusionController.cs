0cbbb2127c28c3c452688422c566a04f97c088e2
Marshmallow Nemo <xiongnemo@126.com>
2020-09-04 14:40:25 +0800

    EarthFusion: rename endpoints so the names become RESTful.
    More work should be done to ensure all api compliant with RESTful
    pattern.
    Refer to https://www.restapitutorial.com/lessons/restfulresourcenaming.html

--- Controllers/EarthFusionController.cs
+++ Controllers/EarthFusionController.cs
@@ -18,49 +18,49 @@ namespace EarthFusion.Controllers
     public class EarthFusionController : ControllerBase
     {
         [HttpGet]
-        public Session GetLoginSession(string username, string password)
+        public Session Login(string username, string password)
         {
             Session httpResponse = new Session();
             Logging.Info("request", "Received request for GetLoginSession");
-            Logging.Info("GetLoginSession", "username: " + username);
+            Logging.Info("Login(GetLoginSession)", "username: " + username);
             httpResponse.Date = DateTime.Now;
             int statusCode = (int)HttpStatusCode.OK;
-            Logging.Info("GetLoginSession", "Begins.");
+            Logging.Info("Login(GetLoginSession)", "Begins.");
             if (username == null || password == null)
             {
                 httpResponse.Message = "Login failed. Something you provided is null.";
-                Logging.Info("GetLoginSession", httpResponse.Message);
+                Logging.Info("Login(GetLoginSession)", httpResponse.Message);
                 statusCode = (int)HttpStatusCode.InternalServerError;
             }
             else
             {
                 UserInformation userInformation = SessionHelpers.Login(username, password);
-            if (userInformation == null)
-            {
-                statusCode = (int)HttpStatusCode.Forbidden;
-                httpResponse.Message = "boom";
-            }
-            else
-            {
-                httpResponse.Message = "good";
-                httpResponse.username = userInformation.userName;
-                Random random = new System.Random();
-                string sessionId;
-                while (true)
+                if (userInformation == null)
                 {
-                    // fake SHA1
-                    sessionId = GenericHelpers.GetRandomHexNumber(40);
-                    string sessionKeyName = "EARTH_FUSION_SESSION_" + sessionId;
-                    if (RedisHelpers.GetString(sessionKeyName) == null)
-                    {
-                        RedisHelpers.SetString(sessionKeyName, userInformation.userId.ToString());
-                        // expire time: three hour
-                        RedisHelpers.SetKeyExpireTime(sessionKeyName, 3 * 60 * 60);
-                        break;
-                    }
+                    statusCode = (int)HttpStatusCode.Forbidden;
+                    httpResponse.Message = "boom";
                 }
-                httpResponse.sessionId = sessionId;
-            }
+                else
+                {
+                    httpResponse.Message = "good";
+                    httpResponse.username = userInformation.userName;
+                    Random random = new System.Random();
+                    string sessionId;
+                    while (true)
+                    {
+                        // fake SHA1
+                        sessionId = GenericHelpers.GetRandomHexNumber(40);
+                        string sessionKeyName = "EARTH_FUSION_SESSION_" + sessionId;
+                        if (RedisHelpers.GetString(sessionKeyName) == null)
+                        {
+                            RedisHelpers.SetString(sessionKeyName, userInformation.userId.ToString());
+                            // expire time: three hour
+                            RedisHelpers.SetKeyExpireTime(sessionKeyName, 3 * 60 * 60);
+                            break;
+                        }
+                    }
+                    httpResponse.sessionId = sessionId;
+                }
             }
             httpResponse.StatusCode = statusCode;
             this.HttpContext.Response.StatusCode = statusCode;
@@ -69,14 +69,14 @@ namespace EarthFusion.Controllers
         }
 
         [HttpGet]
-        public UserInformationHttp GetSession(string sessionId)
+        public UserInformationHttp Session(string sessionId)
         {
             UserInformationHttp httpResponse = new UserInformationHttp();
             Logging.Info("request", "Received request for GetSession");
-            Logging.Info("GetSession", "sessionId: " + sessionId);
+            Logging.Info("Session(GetSession)", "sessionId: " + sessionId);
             httpResponse.Date = DateTime.Now;
             int statusCode = (int)HttpStatusCode.OK;
-            Logging.Info("GetSession", "Begins.");
+            Logging.Info("Session(GetSession)", "Begins.");
             UserInformation currentUser = SessionHelpers.Validate(sessionId);
             if (currentUser == null)
             {
@@ -95,7 +95,7 @@ namespace EarthFusion.Controllers
         }
 
         [HttpPost]
-        public RegisterResult RegisterAccount(string username, string password)
+        public RegisterResult Register(string username, string password)
         {
             RegisterResult httpResponse = new RegisterResult();
             httpResponse.Date = DateTime.Now;
@@ -104,13 +104,13 @@ namespace EarthFusion.Controllers
             if (username == null || password == null)
             {
                 httpResponse.Message = "Register failed. Something you provided is null.";
-                Logging.Info("RegisterAccount", httpResponse.Message);
+                Logging.Info("Register(RegisterAccount)", httpResponse.Message);
                 statusCode = (int)HttpStatusCode.InternalServerError;
             }
             else
             {
-                Logging.Info("RegisterAccount", "username: " + username);
-                Logging.Info("RegisterAccount", "Begins.");
+                Logging.Info("Register(RegisterAccount)", "username: " + username);
+                Logging.Info("Register(RegisterAccount)", "Begins.");
                 bool result = SessionHelpers.RegisterWithoutEmail(username, password);
                 httpResponse.Result = result;
                 if (!result)
@@ -130,16 +130,16 @@ namespace EarthFusion.Controllers
         }
 
         [HttpPost]
-        public RegisterResult RegisterAccountAlt(RegisterRequest httpRequest)
+        public RegisterResult RegisterAlt(RegisterRequest httpRequest)
         {
             string username = httpRequest.Username;
             string password = httpRequest.Password;
             RegisterResult httpResponse = new RegisterResult();
             Logging.Info("request", "Received request for RegisterAccount");
-            Logging.Info("RegisterAccountAlt", "username: " + username);
+            Logging.Info("RegisterAlt(RegisterAccountAlt)", "username: " + username);
             httpResponse.Date = DateTime.Now;
             int statusCode = (int)HttpStatusCode.OK;
-            Logging.Info("RegisterAccountAlt", "Begins.");
+            Logging.Info("RegisterAlt(RegisterAccountAlt)", "Begins.");
             bool result = SessionHelpers.RegisterWithoutEmail(username, password);
             httpResponse.Result = result;
             if (!result)
