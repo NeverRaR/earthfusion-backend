5dd670b48f51c1107f441ce71f4793e2b155f364
Marshmallow Nemo <xiongnemo@126.com>
2020-09-07 10:39:03 +0800

    remote deploy script: reformat code

--- web_deployment.py
+++ web_deployment.py
@@ -31,6 +31,11 @@ def _start_earthfusion():
         return "bad"
     return "what"
 
+def _stop_pull_start_earthfusion():
+    line_1 = _kill_earthfusion()
+    line_2 = _start_earthfusion()
+    return (line_1 + '\n' + line_2)
+
 
 @app.route('/api/earthfusion_ctl/kill', methods=['POST'])
 def kill_earthfusion():
@@ -49,9 +54,8 @@ def start_earthfusion():
 
 @app.route('/api/earthfusion_ctl/stop_pull_start', methods=['POST'])
 def stop_pull_start_earthfusion():
-    line_1 = _kill_earthfusion()
-    line_2 = _start_earthfusion()
-    return (line_1 + line_2)
+    return _stop_pull_start_earthfusion()
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=6000, ssl_context=("fullchain.pem", "privkey.pem"))
