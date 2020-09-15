aaa116f1aae621de22732b0fda1dc37ee9568b68
Marshmallow Nemo <xiongnemo@126.com>
2020-09-15 19:42:32 +0800

    feat: OracleTest: now you can pull from table with range given.

--- OracleTests.cs
+++ OracleTests.cs
@@ -93,6 +93,95 @@ namespace Utils
             conn.Close();
             return contents;
         }
+
+        public static List<WktWithName> WktPullTestWithRange(string username, string passwd, string tableName, int rangeStart, int rangeEnd)
+        {
+            // conn to use
+            OracleConnection conn = OracleHelpers.GetOracleConnection(username, passwd, false);
+
+            // List to return
+            List<WktWithName> contents = new List<WktWithName>();
+
+            string testQueryString = "SELECT geom_data from";
+            testQueryString += "(";
+            testQueryString += "select SDO_GEOMETRY.get_wkt(geom) geom_data, rownum r ";
+            testQueryString += "from nemo.";
+            testQueryString += tableName;
+            testQueryString += ")";
+            testQueryString += "where r > " + (rangeStart - 1).ToString() + " and r < " + (rangeEnd + 1).ToString();
+
+
+            // has geom?
+            bool hasGeom = OracleHelpers.IsColumnNameExistInTableName(conn, tableName, "GEOM".ToString());
+            if (!hasGeom)
+            {
+                return contents;
+            }
+
+            // has name?
+            bool hasName = OracleHelpers.IsColumnNameExistInTableName(conn, tableName, "NAME".ToString());
+            if (hasName)
+            {
+                // also grab the name.
+                testQueryString = "SELECT geom_data, real_name from";
+                testQueryString += "(";
+                testQueryString += "select SDO_GEOMETRY.get_wkt(geom) geom_data, name real_name, rownum r ";
+                testQueryString += "from nemo.";
+                testQueryString += tableName;
+                testQueryString += ")";
+                testQueryString += "where r > " + (rangeStart - 1).ToString() + " and r < " + (rangeEnd + 1).ToString();
+            }
+
+            Logging.Info("WktPullTestWithRange", "Constructed query: " + testQueryString);
+
+            // constructs command from string
+            OracleCommand command = new OracleCommand(testQueryString, conn);
+
+            // open db connection
+            conn.Open();
+
+            // then, executes the data reader
+            OracleDataReader reader = command.ExecuteReader();
+            try
+            {
+                while (reader.Read())
+                {
+                    // Console.WriteLine(reader.GetString(0));
+                    string wkt = reader.GetString(0);
+                    string name = null;
+                    if (hasName)
+                    {
+                        // some data still has null string even there is name column
+                        // also, this proves to be useful when we didn't select name from the table.
+                        try
+                        {
+                            name = reader.GetString(1);
+                        }
+                        catch (System.InvalidCastException e)
+                        {
+                            Logging.Warning("WktPullTestWithRange", "Experienced an InvalidCastException");
+                            if (e.ToString().Split(new[] { '\r', '\n' }).FirstOrDefault() == "System.InvalidCastException: Column contains NULL data")
+                            {
+                                Logging.Warning("WktPullTestWithRange", "Experienced a null string");
+                            }
+                            name = null;
+                        }
+                    }
+                    WktWithName temp = new WktWithName();
+                    temp.wkt = wkt;
+                    temp.name = name;
+                    contents.Add(temp);
+                }
+            }
+            finally
+            {
+                // always call Close when done reading.
+                reader.Close();
+            }
+            conn.Close();
+            return contents;
+        }
+
         public static List<WktWithName> NameSearchTest(string username, string passwd, string tableName, string nameOfPlace)
         {
             // conn to use
